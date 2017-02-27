be19468c33ab497167e15918ed7470f94c66c3aa
George Delaney <georgedelaney@edinburghcollege.ac.uk>
2017-02-27 20:36:33 +0000

    ch-ch-ch-changes

--- src/CoreSite/Controllers/HelloWorldController.cs
+++ src/CoreSite/Controllers/HelloWorldController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 // im done
@@ -14,6 +15,13 @@ namespace CoreSite.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
