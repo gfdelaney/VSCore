2beaea4712272f32ce8ef43337107b56159b1e5d
George Delaney <georgedelaney@edinburghcollege.ac.uk>
2017-02-27 22:02:32 +0000

    add data to view via controller

--- src/CoreSite/Controllers/HelloWorldController.cs
+++ src/CoreSite/Controllers/HelloWorldController.cs
@@ -19,9 +19,12 @@ namespace CoreSite.Controllers
 
         // 
         // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
